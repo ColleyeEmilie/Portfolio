6778eae369736bac79c3fd3188b6a22965a06b8f
emilie <colleyeemilie@hotmail.com>
2023-05-29 21:56:59 +0200

    :sparkles: Add projects css

--- wp-content/themes/portfolio/template-projects.php
+++ wp-content/themes/portfolio/template-projects.php
@@ -2,8 +2,11 @@
 <?php get_header(); ?>
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
 <main class="projects">
-    <h2 class="projects__title title">Découvrez toutes mes <b>réalisations !</b></h2>
+    <h2 class="hidden">Tous mes projets</h2>
     <div class="projects__background"></div>
+    <div>
+        <h3 class="projects__title title">Découvrez toutes mes <b>réalisations !</b></h3>
+    </div>
     <div class="projects__wrapper">
         <?php
         $projects = new WP_Query([
@@ -12,14 +15,15 @@
         ]);
         if($projects->have_posts()): while($projects->have_posts()): $projects->the_post();?>
             <section class="projects__content">
+                <h4 class="projects__name hidden">Projet <?= get_the_title(); ?></h4>
                 <a href="<?= get_the_permalink(); ?>">
                     <figure class="projects__fig">
                         <img src="<?= get_field('project_thumbnail')?>" alt="" class="projects__img">
                     </figure>
                     <div class="projects__regroup">
-                        <h3 class="projects__name title"><?= get_the_title(); ?></h3>
+                        <h5 class="projects__name title bold"><?= get_the_title(); ?></h5>
                         <p class="projects__desc"><?= get_field('project_description'); ?></p>
-                        <p class="projects__link">Découvrir le projet</p>
+                        <div class="projects__link"><p>Découvrir le projet</p></div>
                     </div>
                 </a>
             </section>
